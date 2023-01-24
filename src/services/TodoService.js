befddceb1c5d12f77fc67a27952271ead41ae277
pablo <pablo_vitor1707@hotmail.com>
2023-01-23 22:37:08 -0300

    Criação de Api helpers e service.

---
+++ src/services/TodoService.js
@@ -0,0 +1,16 @@
+import { Api } from 'helpers/Api';
+
+const parseResponse = (response) => response.json();
+
+export const TodoService = {
+    getLista: () =>
+    fetch(Api.todoLista(), { method: "GET"}).then(parseResponse),
+    getById: (id) =>
+    fetch(Api.todoById(id), { method: "GET"}).then(parseResponse),
+    create: () =>
+    fetch(Api.createTodo(), { method: "POST"}).then(parseResponse),
+    updateById: (id) =>
+    fetch(Api.updateTodoById(id), { method: "PUT"}).then(parseResponse),
+    deleteById: (id) =>
+    fetch(Api.deleteTodoById(id), { method: "DELETE"}).then(parseResponse),
+}  
